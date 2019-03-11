0e592004b249a878ea4490fb6232f65feae1f0ce
Mathilde <mathilde.guillossou@gmail.com>
2019-03-11 08:28:36 +0100

    add bottom bar navigation with two fragments

--- app/src/main/java/com/mathilde/foodvisor/MainActivity.kt
+++ app/src/main/java/com/mathilde/foodvisor/MainActivity.kt
@@ -1,24 +1,37 @@
 package com.mathilde.foodvisor
 
+import android.net.Uri
 import android.os.Bundle
+import android.widget.FrameLayout
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.mathilde.foodvisor.ui.HomeFragment
+import com.mathilde.foodvisor.ui.SearchFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), HomeFragment.OnFragmenHometInteractionListener,
+    SearchFragment.OnFragmentSearchInteractionListener {
+    override fun onFragmenSearchtInteraction(uri: Uri) {
+
+    }
+
+    override fun onFragmentInteraction(uri: Uri) {
+
+    }
+
+//    private var content: FrameLayout? = null
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                message.setText(R.string.title_home)
+                addFragment(HomeFragment.newInstance())
+//                message.setText(R.string.title_home)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
+                addFragment(SearchFragment.newInstance("yo", "yu"))
+//                message.setText(R.string.title_dashboard)
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -30,5 +43,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+        addFragment(HomeFragment.newInstance())
+    }
+
+    /**
+     * add/replace fragment in container [FrameLayout]
+     */
+    private fun addFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.content, fragment, fragment.javaClass.simpleName)
+            .commit()
     }
 }
